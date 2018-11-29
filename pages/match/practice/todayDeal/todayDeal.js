9fb1d6fa9c00284ce17811eaa8afc3847bdc5691
stuwyp <stuypwang@163.com>
2018-11-29 14:34:46 +0800

    update

---
+++ pages/match/practice/todayDeal/todayDeal.js
@@ -0,0 +1,89 @@
+// pages/match/practice/todayDeal/todayDeal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    entrust_data: [
+      {
+        "name": "TCL集团",
+        "time": "19:07:29",
+        "entrust_price": "2.560",
+        // "average_price": "0.000",
+        "entrust_number": "10000",
+        // "deal_number": "0",
+        "mode": "买入",
+        // "state": "全部成交"
+
+      },
+      {
+        "name": "许继电气",
+        "time": "19:07:06",
+        "entrust_price": "8.960",
+        // "average_price": "0.000",
+        "entrust_number": "13800",
+        // "deal_number": "0",
+        "mode": "卖出",
+        // "state": "全部成交"
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  
+})
