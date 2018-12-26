5f1fcdc69be08e413e5babf37ed7d1753a2582dd
16eyhuang <773457020@qq.com>
2018-12-26 18:05:13 +0800

    首页信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,10 @@
 //index.js
+
+const shenzhengUrl="http://hq.sinajs.cn/list=sz399001"
+const shangzhengUrl = "http://hq.sinajs.cn/list=sh000001"
+const chuangyeUrl = "http://hq.sinajs.cn/list=sz399006"
+const baseUrl = "http://hq.sinajs.cn/list="
+
 Page({
   data: {
     //上证、深证、创业板数据
@@ -140,34 +146,296 @@ Page({
         "code": "002026"
       },
     ],
-    isShow:[true,false,false,false,false,false]
+    isShow:[true,true,true,true,true,false]
   },
   onLoad: function () {
-    
+    /*沪A涨幅榜 */
+    function hu_up(){
+      let str = 'stock_sh_up_d_10'
+      let url = baseUrl +str
+      //console.log(url)
+      let start=str.length
+      start=start+13
+      wx.request({
+        url: url,
+        success(res){
+          //console.log(res)
+          let str=String(res.data)
+          let end=str.length-3
+          str=str.slice(start,end)
+          //console.log(str)
+          let arr=JSON.parse(str)
+          //console.log(arr)
+          let hu_up=[]
+          for(let i=0;i<arr.length;i++){
+            let obj = {}
+            obj.real_code=arr[i][0]
+            obj.name=arr[i][1]
+            obj.code=arr[i][0].slice(2)
+            obj.price=arr[i][3]
+            obj.percent=arr[i][2]
+            obj['from']="SH"
+            hu_up.push(obj)
+          }
+          //console.log(hu_up)
+          that.setData({
+            hu_up
+          })
+        }
+      })
+    }
+    /*沪A跌幅榜 */
+    function hu_down() {
+      let str = 'stock_sh_down_d_10'
+      let url = baseUrl + str
+      //console.log(url)
+      let start = str.length
+      start = start + 13
+      wx.request({
+        url: url,
+        success(res) {
+          //console.log(res)
+          let str = String(res.data)
+          let end = str.length - 3
+          str = str.slice(start, end)
+          //console.log(str)
+          let arr = JSON.parse(str)
+          //console.log(arr)
+          let hu_down = []
+          for (let i = 0; i < arr.length; i++) {
+            let obj = {}
+            obj.real_code = arr[i][0]
+            obj.name = arr[i][1]
+            obj.code = arr[i][0].slice(2)
+            obj.price = arr[i][3]
+            obj.percent = arr[i][2]
+            obj['from'] = "SH"
+            hu_down.push(obj)
+          }
+          //console.log(hu_down)
+          that.setData({
+            hu_down
+          })
+        }
+      })
+    }
+    /*深A涨幅榜 */
+    function shen_up() {
+      let str = 'stock_sz_up_d_10'
+      let url = baseUrl + str
+      //console.log(url)
+      let start = str.length
+      start = start + 13
+      wx.request({
+        url: url,
+        success(res) {
+          //console.log(res)
+          let str = String(res.data)
+          let end = str.length - 3
+          str = str.slice(start, end)
+          //console.log(str)
+          let arr = JSON.parse(str)
+          //console.log(arr)
+          let shen_up = []
+          for (let i = 0; i < arr.length; i++) {
+            let obj = {}
+            obj.real_code = arr[i][0]
+            obj.name = arr[i][1]
+            obj.code = arr[i][0].slice(2)
+            obj.price = arr[i][3]
+            obj.percent = arr[i][2]
+            obj['from'] = "SZ"
+            shen_up.push(obj)
+          }
+          //console.log(shen_up)
+          that.setData({
+            shen_up
+          })
+        }
+      })
+    }
+    /*深A跌幅榜 */
+    function shen_down() {
+      let str = 'stock_sz_down_d_10'
+      let url = baseUrl + str
+      //console.log(url)
+      let start = str.length
+      start = start + 13
+      wx.request({
+        url: url,
+        success(res) {
+          //console.log(res)
+          let str = String(res.data)
+          let end = str.length - 3
+          str = str.slice(start, end)
+          //console.log(str)
+          let arr = JSON.parse(str)
+          //console.log(arr)
+          let shen_down = []
+          for (let i = 0; i < arr.length; i++) {
+            let obj = {}
+            obj.real_code = arr[i][0]
+            obj.name = arr[i][1]
+            obj.code = arr[i][0].slice(2)
+            obj.price = arr[i][3]
+            obj.percent = arr[i][2]
+            obj['from'] = "SH"
+            shen_down.push(obj)
+          }
+          //console.log(shen_down)
+          that.setData({
+            shen_down
+          })
+        }
+      })
+    }
+
+    hu_up()
+    hu_down()
+    shen_up()
+    shen_down()
+    function getHeatBlock(){
+      wx.request({
+        url: 'http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd=C._BKHY&sty=FCCS&st=c&sr=-1&page=1&pageSize=10&js=var QDtMyMEH={rank:[(x)],pages:(pc),total:(tot)}&token=7bc05d0d4c3c22ef9fca8c2a912d779c&jsName=quote_123',
+        success(res){
+          let str=res.data
+          //console.log(str)
+          str=str.slice(19,-18)
+          //console.log(str)
+          let arr=JSON.parse(str)
+          //console.log(arr)
+          for(let i=0;i<arr.length;i++){
+            arr[i]=arr[i].split(',')
+          }
+          for(let i=0;i<heat_block.length;i++){
+            heat_block[i].name=arr[i][2]
+            heat_block[i].percent=arr[i][3]
+            heat_block[i].heat_stock=arr[i][8]
+            heat_block[i].stock_percent=arr[i][9]
+            /*没有stock_price这个数据。。。 */
+          }
+          that.setData({
+            stock_data,
+            heat_block
+          })
+          wx.hideLoading()
+        }
+      })
+    }
+    let that=this
+    let stock_data=that.data.stock_data
+    let heat_block=that.data.heat_block
+    wx.showLoading({
+      title: '加载中',
+    })
+    /*请求上证指数*/
+    wx.request({
+      url: shangzhengUrl,
+      success(res){
+        //console.log(res)
+        let str=res.data
+        let arr=str.split(',')
+        console.log(arr)
+        /*
+          yesterday为昨日报收
+          now为现在数据
+          change为变化
+          percent为变化率
+         */
+        let yesterday=Number.parseFloat(arr[2]).toFixed(2)
+        let now=Number.parseFloat(arr[3]).toFixed(2)
+        let change=(now-yesterday).toFixed(2)
+        let percent=(Math.abs(change)*100/yesterday).toFixed(2)
+        stock_data[0].index=String(now)
+        stock_data[0].change=String(change)
+        stock_data[0].change_percent=String(percent)
+
+        /*请求深证成指*/
+        wx.request({
+          url: shenzhengUrl,
+          success(res) {
+            //console.log(res)
+            let str = res.data
+            let arr = str.split(',')
+            /*
+              yesterday为昨日报收
+              now为现在数据
+              change为变化
+              percent为变化率
+             */
+            let yesterday = Number.parseFloat(arr[2]).toFixed(2)
+            let now = Number.parseFloat(arr[3]).toFixed(2)
+            let change = (now - yesterday).toFixed(2)
+            let percent = (Math.abs(change) * 100 / yesterday).toFixed(2)
+            stock_data[1].index = String(now)
+            stock_data[1].change = String(change)
+            stock_data[1].change_percent = String(percent)
+
+            /*请求创业板指*/
+            wx.request({
+              url: chuangyeUrl,
+              success(res) {
+                //console.log(res)
+                let str = res.data
+                let arr = str.split(',')
+                /*
+                  yesterday为昨日报收
+                  now为现在数据
+                  change为变化
+                  percent为变化率
+                 */
+                let yesterday = Number.parseFloat(arr[2]).toFixed(2)
+                let now = Number.parseFloat(arr[3]).toFixed(2)
+                let change = (now - yesterday).toFixed(2)
+                let percent = (Math.abs(change) * 100 / yesterday).toFixed(2)
+                stock_data[2].index = String(now)
+                stock_data[2].change = String(change)
+                stock_data[2].change_percent = String(percent)
+                /*请求热门板块 */
+                getHeatBlock()
+              }
+            })
+          }
+        })
+      }
+    })
   },
-  // onShow: function () {
-  //   let that = this
-  //   wx.request({
-  //     url: 'https://easy-mock.com/mock/5a69dacd09247d03931a4dee/example/stock_data', //仅为示例，并非真实的接口地址
-  //     data: {
-  //     },
-  //     method: "GET",
-  //     header: {
-  //       // "Content-Type": "application/x-www-form-urlencoded"  //post
-  //     },
-  //     success: function (res) {
-  //       console.log(res.data)
-  //       that.setData({
-  //         stock_data: res.data.data.stock_data,
-  //         heat_block: res.data.data.heat_block,
-  //         some_stock: res.data.data.some_stock,
-  //       })
-  //     },
-  //     fail: function (err) {
-  //       console.log(err)
-  //     }
-  //   })
-  // },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    wx.reLaunch({
+      url: 'index',
+    })
+  },
+
+  onShow: function () {
+    //每隔5分钟请求一次
+    // setInterval(function(){
+    //   console.log('a')
+    // },1000*60*5)
+    // let that = this
+    // wx.request({
+    //   url: 'https://easy-mock.com/mock/5a69dacd09247d03931a4dee/example/stock_data', //仅为示例，并非真实的接口地址
+    //   data: {
+    //   },
+    //   method: "GET",
+    //   header: {
+    //     // "Content-Type": "application/x-www-form-urlencoded"  //post
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data)
+    //     that.setData({
+    //       stock_data: res.data.data.stock_data,
+    //       heat_block: res.data.data.heat_block,
+    //       some_stock: res.data.data.some_stock,
+    //     })
+    //   },
+    //   fail: function (err) {
+    //     console.log(err)
+    //   }
+    // })
+  },
 
   show:function(e){
     let that=this
@@ -198,14 +466,17 @@ Page({
     })
   },
 
-  toIndexDetail: function() {
+  toIndexDetail: function(e) {
+    let name=e.currentTarget.dataset.name
     wx.navigateTo({
-      url: '/pages/stock/index_detail/index_detail',
+      url: '/pages/stock/index_detail/index_detail?name='+name,
     })
   },
-  toStockDetail: function () {
+  toStockDetail: function (e) {
+    let code=e.currentTarget.dataset.code
+    let name=e.currentTarget.dataset.name
     wx.navigateTo({
-      url: '/pages/stock/stock_detail/stock_detail',
+      url: '/pages/stock/stock_detail/stock_detail?code='+code+'&name='+name,
     })
   },
 })
--- pages/stock/index_detail/index_detail.js
+++ pages/stock/index_detail/index_detail.js
@@ -1,18 +1,115 @@
 // pages/stock/index_detail/index_detail.js
+const shenzhengUrl = "http://hq.sinajs.cn/list=sz399001"
+const shangzhengUrl = "http://hq.sinajs.cn/list=sh000001"
+const chuangyeUrl = "http://hq.sinajs.cn/list=sz399006"
+
+const imageBaseUrl="http://image.sinajs.cn/newchart/"
+const test = "http://image.sinajs.cn/newchart/min/n/sh000001.gif"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isSelected:[true,false,false,false],
+    image: "http://image.sinajs.cn/newchart/min/n/sh000001.gif",
+    fenshi:"http://image.sinajs.cn/newchart/min/n/sh000001.gif",
+    rik:"http://image.sinajs.cn/newchart/daily/n/sh000001.gif",
+    zhouk:"http://image.sinajs.cn/newchart/weekly/n/sh000001.gif",
+    yuek:"http://image.sinajs.cn/newchart/monthly/n/sh000001.gif"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showLoading({
+      title: '加载中',
+    })
+    let that=this
+    let url
+    let image
+    let fenshi
+    let rik
+    let zhouk
+    let yuek
+    if(options.name=="上证指数"){
+      url = shangzhengUrl
+      wx.setNavigationBarTitle({
+        title: '上证指数',
+      })
+      image = "http://image.sinajs.cn/newchart/min/n/sh000001.gif"
+      fenshi = "http://image.sinajs.cn/newchart/min/n/sh000001.gif"
+      rik = "http://image.sinajs.cn/newchart/daily/n/sh000001.gif"
+      zhouk = "http://image.sinajs.cn/newchart/weekly/n/sh000001.gif"
+      yuek = "http://image.sinajs.cn/newchart/monthly/n/sh000001.gif"
+    }
+    else if(options.name=="深证成指"){
+      url = shenzhengUrl
+      wx.setNavigationBarTitle({
+        title: '深证成指',
+      })
+      image = "http://image.sinajs.cn/newchart/min/n/sz399001.gif"
+      fenshi= "http://image.sinajs.cn/newchart/min/n/sz399001.gif"
+      rik ="http://image.sinajs.cn/newchart/daily/n/sz399001.gif"
+      zhouk = "http://image.sinajs.cn/newchart/weekly/n/sz399001.gif"
+      yuek = "http://image.sinajs.cn/newchart/monthly/n/sz399001.gif"
+    }
+    else if(options.name=="创业板指"){
+      url = chuangyeUrl
+      wx.setNavigationBarTitle({
+        title: '创业板指',
+      })
+      image = "http://image.sinajs.cn/newchart/min/n/sz399006.gif"
+      fenshi = "http://image.sinajs.cn/newchart/min/n/sz399006.gif"
+      rik = "http://image.sinajs.cn/newchart/daily/n/sz399006.gif"
+      zhouk = "http://image.sinajs.cn/newchart/weekly/n/sz399006.gif"
+      yuek = "http://image.sinajs.cn/newchart/monthly/n/sz399006.gif"
+    }
+    /*请求对应指数*/
+    wx.request({
+      url: url,
+      success(res) {
+        //console.log(res)
+        let str = res.data
+        let arr = str.split(',')
+        /*
+          yesterday为昨日报收
+          now为现在数据
+          change为变化
+          percent为变化率
+          high为最高点
+          low为最低点
+          number为交易量
+          value为交易额
+         */
+        let yesterday = Number.parseFloat(arr[2]).toFixed(2)
+        let now = Number.parseFloat(arr[3]).toFixed(2)
+        let change = (now - yesterday).toFixed(2)
+        let percent = (Math.abs(change) * 100 / yesterday).toFixed(2)
+        let high=Number.parseFloat(arr[4]).toFixed(2)
+        let low=Number.parseFloat(arr[5]).toFixed(2)
+        let number=(Number.parseFloat(arr[8])/100000000).toFixed(2)
+        let value=(Number.parseFloat(arr[9]/100000000)).toFixed(1)
+        that.setData({
+          yesterday,
+          now,
+          change,
+          percent,
+          high,
+          low,
+          number,
+          value,
+          image,
+          fenshi,
+          rik,
+          zhouk,
+          yuek,
+        })
+        wx.hideLoading()
+      }
+    })
   },
 
   /**
@@ -64,5 +161,29 @@ Page({
 
   },
   
+  switchItem:function(e){
+    let item=Number.parseInt(e.currentTarget.dataset.item)
+    let that=this
+    let isSelected = that.data.isSelected
+    if(isSelected[item]==true)
+      return
+    for(let i=0;i<isSelected.length;i++){
+      isSelected[i]=false
+    }
+    isSelected[item]=true
+    let image
+    if(item==0)
+      image=that.data.fenshi
+    else if(item==1)
+      image=that.data.rik
+    else if(item==2)
+      image=that.data.zhouk
+    else if(item==3)
+      image=that.data.yuek
+    that.setData({
+      isSelected,
+      image
+    })
+  },
 
 })
